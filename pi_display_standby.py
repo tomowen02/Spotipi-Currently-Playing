ce4e5ac67583120a75acbab6573a376ecf70f87f
Tom Owen <tom@tomiowen.com>
2023-05-10 19:53:44 +0100

    Add support for offical pi display enable/ disable

--- pi_display_standby.py
+++ pi_display_standby.py
@@ -1,7 +1,9 @@
 import os
 
 def normal():
-    os.system("DISPLAY=:0 xrandr --output HDMI-1 --auto")
+    #os.system("DISPLAY=:0 xrandr --output HDMI-1 --auto") # Uncomment this if using HDMI
+    #os.system("sudo echo 0 | sudo tee /sys/class/backlight/10-0045/bl_power") # Uncomment if using rpi official display
 
 def no_track():
-    os.system("DISPLAY=:0 xrandr --output HDMI-1 --off")
+    #os.system("DISPLAY=:0 xrandr --output HDMI-1 --off") # Uncomment this if using HDMI
+    #os.system("sudo echo 0 | sudo tee /sys/class/backlight/10-0045/bl_power") # Uncomment if using rpi official display
